68fcaeecf6fe1d98a22594695f87d58cb183fa82
emanuellopess <lopes.emanuel@linknacional.com>
2022-03-30 16:16:27 -0300

    improvement: Definida constante com caminho para traduçoes

--- wc-invoice-payment.php
+++ wc-invoice-payment.php
@@ -36,6 +36,7 @@ if (!defined('WPINC')) {
  * Rename this for your plugin and update it as you release new versions.
  */
 define('WC_PAYMENT_INVOICE_VERSION', '1.0.0');
+define('WC_PAYMENT_INVOICE_TRANSLATION_PATH', plugin_dir_path(__FILE__) . 'languages/');
 
 /**
  * The code that runs during plugin activation.
