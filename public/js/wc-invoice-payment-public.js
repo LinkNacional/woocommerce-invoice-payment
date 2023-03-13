272fb68314e74da4ecd38de41e428846d9d0f8e7
Fabricio de Queiroz <queiroz.fabricio@linknacional.com>
2023-03-13 16:47:31 +0000

    fix: Correção bug de seleção de métodos de pagamentos #12

--- public/js/wc-invoice-payment-public.js
+++ public/js/wc-invoice-payment-public.js
@@ -7,22 +7,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const inputPaymethod = document.getElementById('payment_method_' + defaultPaymethod)
 
-    let otherPaymethod
+    const listPaymethods = document.getElementsByTagName('ul')[10].querySelectorAll('li')
 
-    if (defaultPaymethod === 'bacs') {
-      otherPaymethod = ['cheque', 'cod']
-    } else if (defaultPaymethod === 'cheque') {
-      otherPaymethod = ['bacs', 'cod']
-    } else if (defaultPaymethod === 'cod') {
-      otherPaymethod = ['cheque', 'bacs']
+    let otherPaymethods = []
+
+    for (let i = 0; i < listPaymethods.length; i++) {
+      const temp = (listPaymethods[i].getElementsByTagName('input'))[0].value
+      otherPaymethods.push(temp)
     }
 
-    for (let i = 0; i < otherPaymethod.length; i++) {
-      otherPaymethod[i] = document.getElementsByClassName('wc_payment_method payment_method_' + otherPaymethod[i])
+    otherPaymethods = otherPaymethods.filter(Paymethods => Paymethods !== (String(defaultPaymethod)))
+
+    for (let i = 0; i < otherPaymethods.length; i++) {
+      otherPaymethods[i] = document.getElementsByClassName('wc_payment_method payment_method_' + otherPaymethods[i])
     }
 
-    for (let i = 0; i < otherPaymethod.length; i++) {
-      otherPaymethod[i][0].remove()
+    for (let i = 0; i < otherPaymethods.length; i++) {
+      otherPaymethods[i][0].remove()
     }
 
     if (inputPaymethod) {
