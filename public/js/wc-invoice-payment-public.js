2c8747819770f99d47b95e4e663c295a90304e93
Fabricio de Queiroz <queiroz.fabricio@linknacional.com>
2023-03-13 13:22:14 +0000

    fix: Correção do bug de seleção de métodos de pagamentos na página final #12

--- public/js/wc-invoice-payment-public.js
+++ public/js/wc-invoice-payment-public.js
@@ -7,6 +7,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const inputPaymethod = document.getElementById('payment_method_' + defaultPaymethod)
 
+    let otherPaymethod
+
+    if (defaultPaymethod === 'bacs') {
+      otherPaymethod = ['cheque', 'cod']
+    } else if (defaultPaymethod === 'cheque') {
+      otherPaymethod = ['bacs', 'cod']
+    } else if (defaultPaymethod === 'cod') {
+      otherPaymethod = ['cheque', 'bacs']
+    }
+
+    for (let i = 0; i < otherPaymethod.length; i++) {
+      otherPaymethod[i] = document.getElementsByClassName('wc_payment_method payment_method_' + otherPaymethod[i])
+    }
+
+    for (let i = 0; i < otherPaymethod.length; i++) {
+      otherPaymethod[i][0].remove()
+    }
+
     if (inputPaymethod) {
       inputPaymethod.click()
     }
